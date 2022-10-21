62d3bb43a46447573c199d712fd4005904c9f579
orenSampson <oren.sampson@gmail.com>
2022-10-21 22:53:27 +0300

    csv file added + csv file read to javascript array

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,18 @@ const express = require("express");
 
 const app = express();
 
+let csvData = require("fs").readFileSync("data.csv", "utf8");
+
+csvData = csvData.split("\r\n");
+
+for (let i = 1; i < csvData.length; i++) {
+  csvData[i] = csvData[i].split(",");
+}
+
+csvData.shift();
+
+console.log(csvData);
+
 // app start configuration
 const PORT = process.env.PORT || 8080;
 
